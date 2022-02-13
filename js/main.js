77df21f3a032295e7d98699cac429446a0feef46
Korneliu <olbiton@gmail.com>
2022-02-12 21:17:53 -0800

    inital commit hash routing

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+
+
+
+/*
+var $nutritionList = document.querySelector('#nutrition-list')
+var $sugar= document.querySelector('#sugar')
+var $addButton = document.querySelector('.add-button')
+var $create = document.querySelector('.create')
+var $form = document.querySelector('#nutrition-list')
+
+var testString= ''
+function handleOnClick(event) {
+  event.preventDefault()
+  console.log("event.target.value",event.target.value)
+}
+var caloriesCount = 0
+
+function handleButtonClick(event) {
+  event.preventDefault()
+  console.log("event.target",event.target)
+  console.log("testString",testString)
+  if (testString === 'calories' && caloriesCount < 1) {
+    caloriesCount ++
+    var $option = document.createElement('input')
+    var $optionLabel = document.createElement('label')
+    $optionLabel.textContent= testString
+    $option.setAttribute('class','input-box')
+    $nutritionList.appendChild($optionLabel)
+    $nutritionList.appendChild($option)
+  }
+
+
+}
+function handleNutritionListClick(event) {
+  event.preventDefault()
+  console.log("event.target",event.target.value)
+    testString = event.target.value
+    console.log("testString:",testString)
+  }
+
+function handleFormSubmit(event) {
+  event.preventDefault()
+  console.log('whee')
+}
+
+
+$form.addEventListener("submit",handleFormSubmit)
+$addButton.addEventListener("click",handleButtonClick)
+$sugar.addEventListener("click", handleOnClick)
+//$nutritionList.addEventListener("click",handleNutritionListClick)
+
+var mainRow = document.createElement('div');
+  mainRow.setAttribute('class', 'row');
+  liElement.appendChild(mainRow);
+
+  var entryImage = document.createElement('img');
+  entryImage.setAttribute('class', 'column-full column-half');
+  entryImage.setAttribute('src', entry.photo);
+ */
